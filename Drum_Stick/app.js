a1dd944bec43a25dbe4718fad3d5d5967f38274e
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-09-22 14:01:55 +0530

    Updated code

--- Drum_Stick/app.js
+++ Drum_Stick/app.js
@@ -1,28 +1,49 @@
 //  Running Javascript
 
-const handleAudio = (event) => {
+// A function to remove music after transition gets completed
+const removeMusic = (event) => {
+  if (event.propertyName === "transform") {
+    event.currentTarget.classList.remove("music");
+    // divNode.classList.toggle("music");
+  }
+};
+
+// Function to play associated audio with same data-key attribute and transition div
+const handleMusic = (event) => {
   // Attribute we are looking for
   const attr = "data-key";
 
-  event.stopPropagation();
   let divNode, data;
 
   //   Keyboard event
   if (event.type === "keypress") {
+    // Get key in upper case and then its ascii value
+
     data = event.key.toUpperCase().charCodeAt(0);
+
+    // Find the div node
     divNode = document.querySelector(`div[${attr}="${data}"]`);
   }
   //   It is click event
-  else {
+  else if (event.type === "click") {
+    console.log(this);
     divNode = event.currentTarget;
     data = divNode.dataset.key;
+    console.log(divNode, data);
   }
-
-  document.querySelector(`audio[${attr}="${data}"]`).play();
-  divNode.classList.toggle("music");
-  setTimeout(() => {
-    divNode.classList.toggle("music");
-  }, 1000);
+  // Select associated audio tag
+  const audio = document.querySelector(`audio[${attr}="${data}"]`);
+  if (!audio) {
+    console.log("Audio tag not found !!!");
+  }
+  if (!divNode) {
+    console.log("Div Tag Not found.s");
+  }
+  // Because we want on each event it should restart from begin otherwise if it already playing it will not play again.
+  audio.currentTime = 0;
+  // Play audio
+  audio.play();
+  divNode.classList.add("music");
 };
 
 // Add EventListeners
@@ -31,8 +52,9 @@ document.addEventListener("DOMContentLoaded", function () {
     "Press Any button from middle alphabetical line of Keyboard[A-L] or use mouse to click"
   );
 
-  window.addEventListener("keypress", handleAudio);
-  document
-    .querySelectorAll(".drum")
-    .forEach((node) => node.addEventListener("click", handleAudio));
+  window.addEventListener("keypress", handleMusic);
+  document.querySelectorAll(".drum").forEach((node) => {
+    node.addEventListener("click", handleMusic);
+    node.addEventListener("transitionend", removeMusic);
+  });
 });
